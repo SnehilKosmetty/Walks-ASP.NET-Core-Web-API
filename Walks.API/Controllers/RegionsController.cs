2964e4684edd305e62cbec217dfbb9ab68c72a4f
SnehilKosmetty <Snehilkosmetty21@gmail.com>
2025-01-27 15:17:28 -0500

    Add project files.

---
+++ Walks.API/Controllers/RegionsController.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+using Walks.API.Data;
+using Walks.API.Models.Domain;
+using Walks.API.Models.DTO;
+
+namespace Walks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly WalksDbContext _context;
+        public RegionsController(WalksDbContext dbContext)
+        {
+            _context = dbContext;
+        }
+
+        //GET All Regions
+        //GET : https://localhost:portnumber/api/regions
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            //Get Data From Databse - Domain Models
+            var regionsDomain = _context.Regions.ToList();
+
+            //Map Domain Model to DTOs
+            var regionsDto = new List<RegionDto>();
+            foreach (var region in regionsDomain)
+            {
+                regionsDto.Add(new RegionDto()
+                {
+                    Id = region.Id,
+                    Code = region.Code,
+                    Name = region.Name,
+                    RegionImageUrl = region.RegionImageUrl,
+                });
+            }
+
+
+            //Return DTOs
+            return Ok(regionsDto);
+
+        }
+
+        //Get Single Region(Get Region By ID)
+        //GET : https://localhost:portnumber/api/regions/{id}
+
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public IActionResult GetById([FromRoute] Guid id)
+        {
+            //var region = _context.Regions.Find(id);
+            //Get region Domain model from Database
+            var region = _context.Regions.FirstOrDefault(x => x.Id == id);
+
+            if (region == null)
+            {
+                return NotFound();
+            }
+
+            //Map/Convert Region Domain to Region DTO
+            var regionsDto = new RegionDto
+            {
+                Id = region.Id,
+                Code = region.Code,
+                Name = region.Name,
+                RegionImageUrl = region.RegionImageUrl,
+            };
+
+            //Retrun DTOs Back to client
+            return Ok(regionsDto);
+        }
+
+
+        //Post To Create New Region
+        //Post : https://localhost:portnumber/api/regions
+        [HttpPost]
+        public IActionResult Create([FromBody] AddRegionRequestDto addRegionRequestDto)
+        {
+            //Map or Convert DTO to Domain Model
+            var regionDomainModel = new Region
+            {
+                Code = addRegionRequestDto.Code,
+                Name = addRegionRequestDto.Name,
+                RegionImageUrl = addRegionRequestDto.RegionImageUrl
+            };
+
+            //Use Domain Model to create Region
+            _context.Regions.Add(regionDomainModel);
+            _context.SaveChanges();
+
+
+            //Map Domain model back to DTO
+            var regionDto = new RegionDto
+            {
+                Id = regionDomainModel.Id,
+                Code = regionDomainModel.Code,
+                Name = regionDomainModel.Name,
+                RegionImageUrl = regionDomainModel.RegionImageUrl
+            };
+            return CreatedAtAction(nameof(GetById), new { id = regionDto.Id }, regionDto);
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ Walks.API/Data/WalksDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Walks.API.Models.Domain;
+
+namespace Walks.API.Data
+{
+    public class WalksDbContext : DbContext
+    {
+        public WalksDbContext(DbContextOptions dbContextOptions): base(dbContextOptions)
+        {
+            
+        }
+
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+    }
+}
